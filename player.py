0f6923e26829988f0c33ae593ac7e0f0c8c8c23f
DiegoHdez <128943389+IamSkale@users.noreply.github.com>
2026-03-16 10:56:06 -0400

    Add files via upload

---
+++ player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, player_id: int):
+        self.player_id = player_id
+        
+    def play(self, board: HexBoard) -> tuple:
+        raise NotImplementedError("¡Implementa este método!")
